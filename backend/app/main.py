ef128b59c1b250637c14bc953902ae2869306377
Dylan <dylanconarroe@gmail.com>
2026-08-28 16:44:28 -0700

    added frontend app shell, including household page,components, and data from FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,12 +4,22 @@ from app.database import Base, engine
 from app.models import household, expense, rule, category_mapping
 from app.routers import households, expenses, rules, receipts
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(
     title="HouseSplit API"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
